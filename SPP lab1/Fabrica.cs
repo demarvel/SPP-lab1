4478813d9d479cd0ca4ca207a7921838ad2024d6
demarvel <kharalexey@yandex.ru>
2015-03-17 10:30:34 +0300

    Refactor fabrica to auto

--- SPP lab1/Fabrica.cs
+++ SPP lab1/Fabrica.cs
@@ -6,103 +6,85 @@ using System.Threading.Tasks;
 
 namespace SPP_lab1
 {
-    public abstract class PizzaStore
+    public abstract class AutoPlant
     {
-        public Pizza OrederPizza(string type)
-        {
-            Pizza pizza;
-            pizza = CreatePizza(type);
-            pizza.Prepare();
-            pizza.Bake();
-            pizza.Cute();
-            pizza.Box();
-            return pizza;
-        }
-        abstract Pizza CreatePizza(string type);
+        public abstract Auto CreateAuto(string type);
     }
-    public class NYStore : PizzaStore
+    public class BMW : AutoPlant
     {
-        Pizza CreatePizza(string type)
+        public override Auto CreateAuto(string type)
         {
-            Pizza pizza = null;
-            if (type.Equals("NewYork"))
-            {
-                pizza = new NewYorkPizza();
-            }
-            else if (type.Equals("Americano"))
-            {
-                pizza = new AmericanoPizza();
-            }
-            return pizza;
+            if(type.Equals("i5")){
+                return new BMWi5();
+            } else if (type.Equals("x6")) {
+                return new BMWx6();
+            } else { return null; }
+            
         }
     }
-    public class MexicoStore : PizzaStore
+    public class Audi : AutoPlant
     {
-        Pizza CreatePizza(string type)
+        public override Auto CreateAuto(string type)
         {
-            Pizza pizza = null;
-            if (type.Equals("Americano"))
+            if (type.Equals("A6"))
             {
-                pizza = new AmericanoPizza();
+                return new AudiA6();
             }
-            else if (type.Equals("Mexico"))
+            else if (type.Equals("R8"))
             {
-                pizza = new MexicoPizza();
+                return new AudiR8();
             }
-            return pizza;
+            else { return null; }
+
+        }
+    }
+    public abstract class Auto
+    {
+        public abstract string GetType();
+    }
+    //BMW
+    public class BMWi5 : Auto
+    {
+        public override string GetType()
+        {
+            return "BMW серии i5";
+        }
+    }
+    public class BMWx6 : Auto
+    {
+        public override string GetType()
+        {
+            return "BMW серии x6";
+        }
+    }
+    //Audi
+    public class AudiA6 : Auto
+    {
+        public override string GetType()
+        {
+            return "Audi серии A6";
+        }
+    }
+    public class AudiR8 : Auto
+    {
+        public override string GetType()
+        {
+            return "Audi серии R8";
         }
     }
 
-    public class FabricPizza
-    {
-        public Pizza CreatePizza(string type)
-        {
-            Pizza pizza = null;
-            if (type.Equals("NewYork"))
-            {
-                pizza = new NewYorkPizza();
-            }
-            else if (type.Equals("Americano"))
-            {
-                pizza = new AmericanoPizza();
-            }
-            else if (type.Equals("Mexico"))
-            {
-                pizza = new MexicoPizza();
-            }
-            return pizza;
+    public class MainApp {
+        public static void Main(){
+
+            AutoPlant bmw = new BMW();
+            Auto bmwi5 = bmw.CreateAuto("i5");
+            AutoPlant audi = new Audi();
+            Auto audir8 = audi.CreateAuto("R8");
+
+            string i5 = bmwi5.GetType();
+            string r8 = audir8.GetType();
         }
     }
-    public abstract class Pizza
-    {
-        public string name = "Pizza classic";
-        public string bake = "выпекать при 360 гр.";
-        public string cute = "бла бла бла";
-        public string box = "коробка стандартная";
-        public string Prepare() { return this.name; }
-        public string Bake() { return this.bake; }
-        public string Cute() { return this.cute; }
-        public string Box() { return this.box; }
-    }
-    public class NewYorkPizza : Pizza
-    {
-        public string name = "NewYork Pizza";
-        public string bake = "выпекать при 330 гр.";
-        public string cute = "тонкая корочка";
-        public string box = "коробка из старбакса";
-    }
-    public class AmericanoPizza : Pizza
-    {
-        public string name = "Americano Pizza";
-        public string bake = "выпекать при 460 гр.";
-        public string cute = "корка средней прожаренности";
-        public string box = "коробка со стилем кофе";
-    }
-    public class MexicoPizza : Pizza
-    {
-        public string name = "Mexico Pizza";
-        public string bake = "выпекать при 260 гр.";
-        public string cute = "толстая корка";
-        public string box = "на коробке нарисован Эль-Мучачос";
-    }
+
+}
 }
